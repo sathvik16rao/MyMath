f5f5cdaa9ee019d44dd364d3093de0422a8f8c98
sathvik16rao <sathvik16rao@gmail.com>
2022-12-30 15:57:49 +0530

    Minor changes

--- main_page.py
+++ main_page.py
@@ -1,13 +1,13 @@
 from tkinter import *
 import MyMath
 
-root = Tk()
-root.title('Scientific Calculator')
-root.config(bg='#000000')
-root.geometry('450x335')
-root.resizable(FALSE,FALSE)
+main = Tk()
+main.title('Scientific Calculator')
+main.config(bg='#000000')
+main.geometry('450x335')
+main.resizable(FALSE,FALSE)
 
-entryField = Entry(root, font=('times new roman',20,'bold'), bg='#000000', fg='#F1DDCF', bd=5, relief=RIDGE, width = 31, insertbackground = 'white')
+entryField = Entry(main, font=('times new roman',20,'bold'), bg='#000000', fg='#F1DDCF', bd=5, relief=RIDGE, width = 31, insertbackground = 'white')
 entryField.grid(row=0,column=0,columnspan=8)
 
 def click(value):
@@ -20,7 +20,7 @@ def click(value):
         elif value=='AC':
             entryField.delete(0,END)
         elif value=="√":
-            result = MyMath.sqroot(eval(val))
+            result = MyMath.sqmain(eval(val))
             entryField.delete(0,END)
             entryField.insert(0, result)
         elif value=="π":
@@ -55,8 +55,8 @@ def click(value):
             result = MyMath.cot(MyMath.radians(eval(val)))
             entryField.delete(0,END)
             entryField.insert(0, result)
-        elif value==chr(8731):                  #cube root
-            result = MyMath.nthroot(eval(val),3)
+        elif value==chr(8731):                  #cube main
+            result = MyMath.nthmain(eval(val),3)
             entryField.delete(0,END)
             entryField.insert(0, result)
         elif value=="x\u02b8":                  #x^y
@@ -113,22 +113,19 @@ button_text_list = ["Del", "AC", "√", "+", "π", "sinθ", "cosθ", "tanθ",
                     "4", "5", "6", "×", chr(8731), "x\u02b8", "x\u00B3", "x\u00B2",
                     "7", "8", "9", chr(247), "ln", "Deg", "Rad", "e",
                     "0", ".", "%", "=", "log", "(", ")", "x!"]
-button_bg1 = '#faba0a'
-button_bg2 = '#0eab8c'
-button_bg3 = '#98d8ce'
+
 rowvalue = 1
 columnvalue = 0
 for i in button_text_list:
     if i in '0123456789':
-        button_clear = Button(root, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg=button_bg1 , activebackground='#CCA01D', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
+        button = Button(main, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg='#faba0a' , activebackground='#CCA01D', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
     elif i in ["Del", "AC", "√", "+", "-", "×",chr(247), ".", "%", "="]:
-        button_clear = Button(root, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg=button_bg3 , activebackground='#c3dad4', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
+        button = Button(main, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg='#98d8ce' , activebackground='#c3dad4', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
     else:
-        button_clear = Button(root, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg=button_bg2 , activebackground='#65a897', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
+        button = Button(main, font=('calibri', 12 ,'bold'), width=5, height=2, bd=4, relief=RIDGE, text=i, bg='#0eab8c' , activebackground='#65a897', command=lambda button=i: click(button)).grid(row=rowvalue, column=columnvalue, padx=1,pady=1)
     columnvalue += 1
     if columnvalue > 7:
         rowvalue += 1
         columnvalue = 0
 
-
-root.mainloop()
+main.mainloop()
