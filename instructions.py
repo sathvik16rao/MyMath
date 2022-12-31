c57a76e9f40af0c91e0d25a5c0b4769244679016
sathvik16rao <sathvik16rao@gmail.com>
2022-12-31 13:06:54 +0530

    Added new rules

--- instructions.py
+++ instructions.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title('Instuctions')
-root.geometry('1010x220')
+root.geometry('1010x280')
 root.resizable(FALSE,FALSE)
 
 l1 = Label(root, text='How to use the calculator', font=('Comic Sans MS',20,'bold'))
@@ -20,10 +20,16 @@ l4.place(x = 20, y = 100)
 l5 = Label(root,text='\u2022 For nCr and nPr, input in the same format and click on the respective button for the result (Eg: 6C2 or 6c2 or 6P2 or 6p2)', font=('Times new roman',15))
 l5.place(x = 20, y = 125)
 
-l6 = Label(root,text='\u2022 For LCM and HCF, input all th numbers seperated by commas and clickon the respective button for the result.', font=('Times new roman',15))
+l6 = Label(root,text='\u2022 For LCM and HCF, input all th numbers seperated by commas and click on the respective button for the result.', font=('Times new roman',15))
 l6.place(x = 20, y = 150)
 
+l7 = Label(root,text='\u2022 For logn, input in the format "number,base" and click on the respective button for result.', font=('Times new roman',15))
+l7.place(x = 20, y = 175)
+
+l8 = Label(root,text='\u2022 For \u207F√, input in the format "number,n" and click on the respective button for the result.', font=('Times new roman',15))
+l8.place(x = 20, y = 200)
+
 b1 = Button(root, text="Let's Go!", command=root.destroy, font=('calibri', 12 ,'bold'), width=10, height=1, bd=4, relief=RAISED)
-b1.place(x = 450, y = 175)
+b1.place(x = 450, y = 225)
 
 root.mainloop()
