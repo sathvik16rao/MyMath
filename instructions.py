e308c61fe29bedc4ac5a3fc28fd3c3e4e8cce0d0
sathvik16rao <sathvik16rao@gmail.com>
2023-01-15 10:28:11 +0530

    Updated angle rules

--- instructions.py
+++ instructions.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title('Instuctions')
-root.geometry('1010x300')
+root.geometry('1010x330')
 root.resizable(FALSE,FALSE)
 
 l1 = Label(root, text='How to use the calculator', font=('Comic Sans MS',20,'bold'))
@@ -22,7 +22,7 @@ l5.place(x = 20, y = 125)
 lsub1 = Label(root, text='(Eg: 6C2 or 6c2 or 6P2 or 6p2 or 6,2)', font=('Times new roman',15))
 lsub1.place(x=45,y=150)
 
-l6 = Label(root,text='\u2022 For LCM and HCF, input all th numbers seperated by commas and click on the respective button for the result.', font=('Times new roman',15))
+l6 = Label(root,text='\u2022 For LCM and HCF, input all the numbers seperated by commas and click on the respective button for the result.', font=('Times new roman',15))
 l6.place(x = 20, y = 175)
 
 l7 = Label(root,text='\u2022 For logn, input in the format "number,base" and click on the respective button for result.', font=('Times new roman',15))
@@ -31,7 +31,10 @@ l7.place(x = 20, y = 200)
 l8 = Label(root,text='\u2022 For \u207F√, input in the format "number,n" and click on the respective button for the result.', font=('Times new roman',15))
 l8.place(x = 20, y = 225)
 
+l9 = Label(root,text='\u2022 The default angle input/output is Degrees. It can be changed using the angle toggle buttons.', font=('Times new roman',15))
+l9.place(x = 20, y = 250)
+
 b1 = Button(root, text="Let's Go!", command=root.destroy, font=('calibri', 12 ,'bold'), width=10, height=1, bd=4, relief=RAISED)
-b1.place(x = 450, y = 255)
+b1.place(x = 450, y = 280)
 
 root.mainloop()
